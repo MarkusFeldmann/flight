c787860e2ae9ba0eb40cc702ece136c654c4f3c0
MarkusFeldmann <mf@mind-it.co>
2017-09-11 17:51:56 +0200

    new

---
+++ src/app/flight-search/flight-search.component.ts
@@ -0,0 +1,48 @@
+import { Flight } from '../entities/flight';
+import { Component, OnInit } from '@angular/core';
+import { Http, Headers, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'flight-search',
+  templateUrl: './flight-search.component.html',
+  styleUrls: ['./flight-search.component.css']
+})
+export class FlightSearchComponent {
+
+  public from: string;
+  public to: string;
+  public date: string = (new Date()).toISOString();
+  public flights: Array<Flight>[];
+  selectedFlight: Flight;
+
+  constructor(private http: Http) { }
+
+  search() {
+    let url = "http://www.angular-akademie.com/api/flight";
+
+    let headers = new Headers();
+    headers.set('Accept', 'text/json');
+
+    let search = new URLSearchParams();
+    search.set('from', this.from);
+    search.set('to', this.to);
+
+    this.http
+      .get(url, { headers, search })
+      .map(resp => resp.json())
+      .subscribe(flights => {
+        this.flights = flights;
+      },
+      err => {
+        console.log(err);
+      }
+      );
+  }
+
+  select(f: Flight) {
+    this.selectedFlight = f;
+  }
+
+
+}
